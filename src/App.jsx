accbf18a1a08bd945a5997a3a7d499db4b7dc839
letitia-chiu <hsuan.acc@outlook.com>
2024-02-13 19:05:01 +0800

    build: add react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import { ThemeProvider } from '@emotion/react'
 import theme from './theme'
+import { BrowserRouter, Route, Routes} from 'react-router-dom'
 
 // Views
 import IncomeListPage from './views/IncomeListPage'
@@ -13,8 +14,9 @@ function App() {
       setIsMobile(window.innerWidth < 960)
     }
 
-    // Initial check
+    // Initial check screen size
     checkScreenSize()
+    
     // Set up event listener
     window.addEventListener('resize', checkScreenSize)
 
@@ -27,7 +29,19 @@ function App() {
   return (
     <ThemeProvider theme={theme.light}>
       <div className="App">
-        <IncomeListPage isMobile={isMobile} />
+        <BrowserRouter>
+          <Routes>
+            <Route
+              path="/income"
+              element={<IncomeListPage isMobile={isMobile} />}
+            />
+            <Route
+              path="*"
+              element={<div>Simple Expense Manager</div>}
+            />
+          </Routes>
+        </BrowserRouter>
+        
       </div>
     </ThemeProvider>
   )
