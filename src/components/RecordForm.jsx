64b4c9ebbb3f97ba21d426845bf069fffd772e76
letitia-chiu <hsuan.acc@outlook.com>
2024-02-13 22:31:33 +0800

    RecordForm - add input handler

--- src/components/RecordForm.jsx
+++ src/components/RecordForm.jsx
@@ -8,41 +8,82 @@ import {
 } from '@chakra-ui/react'
 import { CheckIcon, CloseIcon } from '@chakra-ui/icons'
 
-function RecordForm({ mode, page }) {
-  let isError = false
+const dummyCat = [
+  {
+    id: 6,
+    name: 'Salary'
+  },
+  {
+    id: 7,
+    name: 'Bonus'
+  }
+]
+
+function RecordForm({ page, record }) {
+  const [dateInput, setDateInput] = useState(() => record?.date || dayjs().format('YYYY-MM-DD'))
+  const handleDateChange = e => {
+    setDateInput(e.target.value)
+  }
+
+  const [titleInput, setTitleInput] = useState(() => record?.title || '')
+  const handleTitleChange = e => {
+    setTitleInput(e.target.value)
+  }
+
+  const [categoryInput, setCategoryInput] = useState(() => record?.categoryId || '')
+  const handleCategoryChange = e => {
+    setCategoryInput(e.target.value)
+  }
+
+  const [amountInput, setAmountInput] = useState(() => record?.amount || '')
+  const handleAmountChange = e => {
+    setAmountInput(e.target.value)
+  }
 
   return (
     <Stack w='90%' my={3} spacing={5}>
       <FormControl>
         <FormLabel>Date</FormLabel>
-        <Input type="date" defaultValue={dayjs().format('YYYY-MM-DD')}/>
+        <Input
+          type='date'
+          value={dateInput} 
+          onChange={handleDateChange}
+        />
       </FormControl>
 
-      <FormControl isInvalid={isError}>
+      <FormControl>
         <FormLabel>Title</FormLabel>
-        <Input type='text' placeholder='Enter title' />
-        {isError && 
-          <FormErrorMessage>Title is required.</FormErrorMessage>
-        }
+        <Input
+          type='text'
+          placeholder='Enter title'
+          value={titleInput}
+          onChange={handleTitleChange}
+        />
       </FormControl>
 
       <FormControl>
         <FormLabel>Category</FormLabel>
-        <Select placeholder='Select category'>
-          <option value='option1'>Option 1</option>
-          <option value='option2'>Option 2</option>
-          <option value='option3'>Option 3</option>
+        <Select
+          placeholder='Select category'
+          value={categoryInput}
+          onChange={handleCategoryChange}
+        >
+          {dummyCat.map(c => (
+            <option key={c.id} value={c.id}>{c.name}</option>
+          ))}
         </Select>
       </FormControl>
 
-      <FormControl isInvalid={isError}>
+      <FormControl>
         <FormLabel>Amount</FormLabel>
-        <NumberInput>
-          <NumberInputField placeholder='Enter amount'/>
+        <NumberInput
+          value={amountInput}
+        >
+          <NumberInputField
+            placeholder='Enter amount'
+            onChange={handleAmountChange}
+          />
         </NumberInput>
-        {isError && 
-          <FormErrorMessage>Amount is required.</FormErrorMessage>
-        }
       </FormControl>
 
       <ButtonGroup justifyContent='end' spacing={5} mt={5}>
